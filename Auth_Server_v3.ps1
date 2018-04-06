25b09f1dcee29b88551e6803b6b67affcda00343
marceleisenhut <37595033+marceleisenhut@users.noreply.github.com>
2018-04-06 12:05:47 +0200

    Add files via upload

--- Auth_Server_v3.ps1
+++ Auth_Server_v3.ps1
@@ -1,11 +1,42 @@
-﻿
+﻿<#
+.Synopsis
+   Starts Authentification Server for Arduino PIN and RFID Access Control
+.DESCRIPTION
+    This Scripts Authentificates RFID + PINs which are send from an Arduino Serial Connection against a csv database.
+    Stop the Server with [Ctrl] + [Q]
+.EXAMPLE
+   .\Auth_Server_v3.ps1 -ComPort "COM9" -BaudRate "9600" -Database .\aut_database.csv
+.PARAMETER $ComPort
+    COMPort on Which the Arduino is connected
+.PARAMETER $BaudRate
+    BaudRate for the Serial Connection
+.PARAMETER $Database
+    Path to the CSV Database File
+.NOTES
+    Author:    'Stefan Bosser'
+    Version:   1.0 / 06.04.2018 /
+        - initial
+#>
 
+#-- Script Paramter ------------------------------------------------------------------------------------------------------------------#
+Param(
+    [Parameter(mandatory=$True)]
+    [string]$ComPort,
+
+    [Parameter(mandatory=$false)]
+    [string]$BaudRate = 9600,
+
+    [Parameter(mandatory=$true)]
+    [string]$Database   
+)
+
+#-- region functions -----------------------------------------------------------------------------------------------------------------#
 function ReadSerialLine()
 {
     $input = $null
     try
     {
-        Write-Host "Reading: " -NoNewline
+        Write-Host "Reading                     : " -NoNewline
         do
         {
             $input = $port.ReadLine()
@@ -28,7 +59,7 @@ function writeSerialLine($String)
 {
     try
     {
-        Write-Host "Writting to Serial Port: " $String
+        Write-Host "Writting to Serial Port     :" $String
         $port.Write($String)
     }
 
@@ -62,7 +93,7 @@ function AuthetificateCardAndPin($CardNumber, $PIN)
 {
     $AuthEntry = $authdb | Where-Object{$_.RFID -eq $CardNumber}
 
-    if($AuthEntry.RFID -eq $CardNumber -and $AuthEntry.PIN -eq $PIN -and $AuthEntry.Access -eq $true)
+    if($AuthEntry.RFID -eq $CardNumber -and $AuthEntry.PIN -eq $pin -and $AuthEntry.Access -eq $true)
     {
         return $true
     }
@@ -71,30 +102,47 @@ function AuthetificateCardAndPin($CardNumber, $PIN)
         return $false
     }
 }
+#-- endregion functions --------------------------------------------------------------------------------------------------------------#
+
+#-- region main ----------------------------------------------------------------------------------------------------------------------#
 
 # Create Serial Port Object
 $port = New-Object System.IO.Ports.SerialPort
-$port.PortName = "COM5"
-$port.BaudRate = "9600"
+$port.PortName =$ComPort
+$port.BaudRate = $BaudRate
 $port.Parity = "None"
 $port.DataBits = 8
 $port.StopBits = 1
 $port.ReadTimeout = 1000000 # 9 seconds
 $port.DtrEnable = "true"
 
-# Open Serial Port
-Write-Host "Opening Serial Connection on: " $ComPort
-$port.open() 
-# wait 2 seconds until Arduino is ready
-Start-Sleep 2 
-
+try
+{# Open Serial Port
+    Write-Host "Opening Serial Connection on:" $ComPort
+    $port.open() 
+    # wait 2 seconds until Arduino is ready
+    Start-Sleep 2 
+}
+catch
+{
+   Write-Host "Failed to open Port, please try again" -ForegroundColor Red
+   Write-host "Error: " $Error[0].FullyQualifiedErrorId -ForegroundColor Red
+   sleep 5
+   exit
+}
 # Read Authentification Database
-$AuthDB = import-csv -Delimiter ";" -Path .\aut_database.csv 
+
+$AuthDB = Import-Csv -Delimiter ";" -Path $Database
 
 while($true)
 {
+    $key = if ($host.UI.RawUI.KeyAvailable) 
+        {
+            $host.UI.RawUI.ReadKey('NoEcho, IncludeKeyDown')
+        }
+    
     $authTagString = ReadSerialLine 
-    Write-Host "AuthString:" $authTagString
+    Write-Host "AuthString                  :" $authTagString
     if($authTagString -ne $null)
     {
         $authArray = $authTagString.Replace("`n","").Replace("`r","").split("/")
@@ -102,7 +150,7 @@ while($true)
         switch ($authArray[0])
         {
             "RFID"{$result = AuthentificateCard -CardNumber $authArray[1]}
-            "PIN"{$result = AuthetificateCardAndPin -PIN $authArray[1] -CardNumber $authArray[2]  }
+            "PIN"{$result = AuthetificateCardAndPin -PIN $authArray[1] -CardNumber $authArray[2]}
         }
 
         
@@ -112,8 +160,8 @@ while($true)
             $false{writeSerialLine -String "1"}
             default {}
         }
-        
-        write-host "Result: "$result
+       
+        write-host "Result                      :" $result
 
     }
 
@@ -124,17 +172,19 @@ while($true)
     $authArray = $null
     $result = $null
     sleep 1
-    <#
+    
     if ([console]::KeyAvailable)
     {
-        $key = [system.console]::readkey($true)
-        if (($key.modifiers -band [consolemodifiers]"control") -and ($key.key -eq "C"))
+        $key = $host.UI.RawUI.ReadKey('NoEcho, IncludeKeyDown')
+        if (($key.VirtualKeyCode -eq 81) -AND ($key.ControlKeyState -match "LeftCtrlPressed"))
         {
+            Write-Host "Stopping Server..."
             break
         }
-    }#>
-}
+    }
+} 
 
+#closes serial connection
+$port.Close() 
 
-$port.Close() #closes serial connection
-
+#-- endregion main -------------------------------------------------------------------------------------------------------------------#
